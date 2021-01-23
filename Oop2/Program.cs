52e975bebd8bf9e03b27d4226cc9687058afff68
PC <PC@DESKTOP-QB8PS4K>
2021-01-23 22:04:10 +0300

    Add project files.

---
+++ Oop2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gerçek Müşteri İçin
+            //mustafa sav için
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "123456";
+            musteri1.Adi = "Mustafa";
+            musteri1.Soyadi = "Sav";
+            musteri1.TcNo = "1234567890";
+
+            //Tüzel Müşteri İçin
+            //Kodlama.IO müşteri
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.IO";
+            musteri2.VergiNo = "1234567890";
+
+            // Gerçek Müşteri - Tüzel Müşteri
+            //SOLID 
+
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager musteriManager = new CustomerManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            mus
+
+        }
+    }
+}
---
+++ Oop2/TuzelMusteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    //Coorporate = Tüzel demek
+
+    //Miras - inheratance
+    class TuzelMusteri:Musteri //Tüzel müşteri de müşteriden türemiştir. Müşteride olan herşey tüzeldede olur..
+    {
+
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+
+    }
+}
