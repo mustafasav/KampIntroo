4c0b2125c6149ab883860deca475c71c37a1eda1
PC <PC@ARGE>
2021-01-26 13:19:40 +0300

    26.01.2021

--- Donguler/Program.cs
+++ Donguler/Program.cs
@@ -6,7 +6,43 @@ namespace Donguler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 10; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+
+            
+            // array --- dizi
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya Başlangıç İçin Temel Kurs", 
+                "Java","Python","Kotlin","Flutter" };
+
+
+            Console.WriteLine("\nKurs For Döngüsü Başladı");
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("Kurs For Döngüsü Bitti");
+
+            Console.WriteLine("\nKurs Foreach Döngüsü Başladı");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Kurs Foreach Döngüsü Bitti");
         }
     }
 }
