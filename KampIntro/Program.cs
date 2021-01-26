74ec1d0c1099fee6a7632fae2985c483cbec8885
PC <PC@ARGE>
2021-01-26 11:20:29 +0300

    26.01.2021

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,7 +6,40 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //do not repeat yourself - kendini tekrarlama!!!
+            //type safety - tip güvenliği
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.42;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu Göster");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu Göster");
+            }
+            else
+            {
+                Console.WriteLine("Değişim Yok Butonu Göster");
+            }
+
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+              
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
         }
     }
 }
